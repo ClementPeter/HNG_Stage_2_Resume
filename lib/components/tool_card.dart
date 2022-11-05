5e3b54b70de9fe1448ad1ace5406b95de2831a11
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-11-05 09:43:23 +0100

    HNG_STAGE_2 Mobile Resume

--- lib/components/tool_card.dart
+++ lib/components/tool_card.dart
@@ -52,7 +52,7 @@ Widget toolsCard() {
                   //html.window.open(Constants.DART, 'DART');
                 },
               ),
-              const SizedBox(width: 20),
+              const SizedBox(width: 10),
               ElevatedButton.icon(
                 //hoverColor: Color(0XFFd1e7f9),
                 icon: Padding(
@@ -79,127 +79,127 @@ Widget toolsCard() {
           ),
           const SizedBox(height: 10),
           //Second Layer of Tools
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      //   _launchBehance();
-                      null;
-                    },
-                    //hoverColor: Color(0XFFd1e7f9),
-                    icon: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 10.0, top: 10.0, bottom: 10.0),
-                      child: SizedBox(
-                        height: 25,
-                        width: 25,
-                        child: Image.asset("assets/images/android.png"),
-                      ),
-                    ),
-                    label: const Padding(
-                      padding:
-                          EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
-                      child: Text('Andriod Studio'),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(width: 20),
-              ElevatedButton.icon(
-                onPressed: () {
-                  null;
-                },
-                icon: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 10.0, top: 10.0, bottom: 10.0),
-                  child: SizedBox(
-                    height: 25,
-                    width: 25,
-                    child: Image.asset("assets/images/vscode.png"),
-                  ),
-                ),
-                label: const Padding(
-                  padding:
-                      EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
-                  child: Text('Vscode'),
-                ),
-              ),
-              const SizedBox(width: 20),
-              ElevatedButton.icon(
-                onPressed: () {
-                null;
-                },
-                icon: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 10.0, top: 10.0, bottom: 10.0),
-                  child: SizedBox(
-                    height: 25,
-                    width: 25,
-                    child: Image.asset("assets/images/github.png"),
-                  ),
-                ),
-                label: const Padding(
-                  padding:
-                      EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
-                  child: Text('GitHub'),
-                ),
-              ),
-              const SizedBox(width: 20),
-            ],
-          ),
-          const SizedBox(height: 20),
+          // Row(
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: [
+          //     Row(
+          //       crossAxisAlignment: CrossAxisAlignment.center,
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //       children: [
+          //         ElevatedButton.icon(
+          //           onPressed: () {
+          //             //   _launchBehance();
+          //             null;
+          //           },
+          //           //hoverColor: Color(0XFFd1e7f9),
+          //           icon: Padding(
+          //             padding: const EdgeInsets.only(
+          //                 left: 10.0, top: 10.0, bottom: 10.0),
+          //             child: SizedBox(
+          //               height: 25,
+          //               width: 25,
+          //               child: Image.asset("assets/images/android.png"),
+          //             ),
+          //           ),
+          //           label: const Padding(
+          //             padding:
+          //                 EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
+          //             child: Text('Andriod Studio'),
+          //           ),
+          //         ),
+          //       ],
+          //     ),
+          //     const SizedBox(width: 20),
+          //     ElevatedButton.icon(
+          //       onPressed: () {
+          //         null;
+          //       },
+          //       icon: Padding(
+          //         padding: const EdgeInsets.only(
+          //             left: 10.0, top: 10.0, bottom: 10.0),
+          //         child: SizedBox(
+          //           height: 25,
+          //           width: 25,
+          //           child: Image.asset("assets/images/vscode.png"),
+          //         ),
+          //       ),
+          //       label: const Padding(
+          //         padding:
+          //             EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
+          //         child: Text('Vscode'),
+          //       ),
+          //     ),
+          //     const SizedBox(width: 20),
+          //     ElevatedButton.icon(
+          //       onPressed: () {
+          //       null;
+          //       },
+          //       icon: Padding(
+          //         padding: const EdgeInsets.only(
+          //             left: 10.0, top: 10.0, bottom: 10.0),
+          //         child: SizedBox(
+          //           height: 25,
+          //           width: 25,
+          //           child: Image.asset("assets/images/github.png"),
+          //         ),
+          //       ),
+          //       label: const Padding(
+          //         padding:
+          //             EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
+          //         child: Text('GitHub'),
+          //       ),
+          //     ),
+          //     const SizedBox(width: 20),
+          //   ],
+          // ),
+          //const SizedBox(height: 20),
           //Third Layer of Tools
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                icon: Padding(
-                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: SizedBox(
-                    height: 25,
-                    width: 25,
-                    child: Image.asset(
-                      "assets/images/figma.png",
-                    ),
-                  ),
-                ),
-                label: const Padding(
-                  padding:
-                      EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
-                  child: Text('Adobe XD'),
-                ),
-                onPressed: () {
-                  null;
-                },
-              ),
-              const SizedBox(width: 20),
-              ElevatedButton.icon(
-                icon: Padding(
-                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: SizedBox(
-                    height: 25,
-                    width: 25,
-                    child: Image.asset("assets/images/xd.png"),
-                  ),
-                ),
-                label: const Padding(
-                  padding:
-                      EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
-                  child: Text('Figma'),
-                ),
-                onPressed: () {
-                  null;
-                },
-              ),
-            ],
-          ),
+          // Row(
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: [
+          //     ElevatedButton.icon(
+          //       icon: Padding(
+          //         padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+          //         child: SizedBox(
+          //           height: 25,
+          //           width: 25,
+          //           child: Image.asset(
+          //             "assets/images/figma.png",
+          //           ),
+          //         ),
+          //       ),
+          //       label: const Padding(
+          //         padding:
+          //             EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
+          //         child: Text('Adobe XD'),
+          //       ),
+          //       onPressed: () {
+          //         null;
+          //       },
+          //     ),
+          //     const SizedBox(width: 20),
+          //     ElevatedButton.icon(
+          //       icon: Padding(
+          //         padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+          //         child: SizedBox(
+          //           height: 25,
+          //           width: 25,
+          //           child: Image.asset("assets/images/xd.png"),
+          //         ),
+          //       ),
+          //       label: const Padding(
+          //         padding:
+          //             EdgeInsets.only(right: 10.0, top: 10.0, bottom: 10.0),
+          //         child: Text('Figma'),
+          //       ),
+          //       onPressed: () {
+          //         null;
+          //       },
+          //     ),
+          //   ],
+          // ),
           const SizedBox(height: 20.0),
         ],
       ),
